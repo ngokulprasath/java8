9490eba81ce6e25bba9f8d12eaf2dd7f8443db26
Gokulprasath <aswingokul.mca@gmail.com>
2021-07-28 14:54:16 +0530

    Initial code

---
+++ java8/com/basic/java8/BasicJava.java
@@ -0,0 +1,9 @@
+package com.basic.java8;
+
+class BasicJava {
+
+    public static void main(String[] args) {
+        System.out.println("I can't wait to write java code and push it to github");
+    }
+   
+}
